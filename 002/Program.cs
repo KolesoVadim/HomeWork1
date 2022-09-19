f1c684d98a9ecec82f14be3e1ade058ba23e4fd9
KolesoVadim <vadimkolesnichenko1404@gmail.com>
2022-09-19 14:51:44 +0300

    added 002

---
+++ 002/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write("enter A: ");
+int A = int.Parse(Console.ReadLine()??"0");
+Console.Write("enter B: ");
+int B = int.Parse(Console.ReadLine()??"0");
+Console.Write("enter C: ");
+int C = int.Parse(Console.ReadLine()??"0");
+int max=A;
+if (B > max) max=B;
+if (C > max) max=C;
+Console.Write("max = ");
+Console.WriteLine(max);

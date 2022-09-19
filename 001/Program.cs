2435463258b3992ff6d93d92571d7e62a2a5ffd0
KolesoVadim <vadimkolesnichenko1404@gmail.com>
2022-09-19 14:14:33 +0300

    added 001

---
+++ 001/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Write("ENTER NUMBER A: ");
+int A = int.Parse(Console.ReadLine()??"0");
+Console.Write("ENTER NUMBER B: ");
+int B = int.Parse(Console.ReadLine()??"0");
+if (A > B)
+{
+    Console.WriteLine("number A BIG");
+}
+else
+{
+    Console.WriteLine("number B BIG");
+}

b94b2b16a78601dbc0b4a901a9b7c2705a6b1f25
KolesoVadim <vadimkolesnichenko1404@gmail.com>
2022-09-19 15:45:36 +0300

    added 003

---
+++ 003/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+Console.Write("ENTER NUMBER: ");
+int number = int.Parse(Console.ReadLine()??"0");
+if (number % 2 == 0)
+{
+    Console.Write("number kratno");
+}
+else
+{
+    Console.Write("net");
+}

1f69df868156ab1910e540dfa84f55f7a6589f73
KolesoVadim <vadimkolesnichenko1404@gmail.com>
2022-09-22 16:36:28 +0300

    added 005

---
+++ 005/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Write("Введите число: ");
+string str = Console.ReadLine()??"0";
+ if(str.Length >= 3)
+ {
+     Console.WriteLine(str[2]);
+ }
+ else
+ {
+     Console.WriteLine("Третьей цифры нет");
+ }

b1d3d9045f515de1a31174761bee599aa2c63b02
KolesoVadim <vadimkolesnichenko1404@gmail.com>
2022-09-29 16:28:16 +0300

    added 011

---
+++ 011/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Write("Enter your number: ");
+int number = int.Parse(Console.ReadLine()??"0");
+int sum = 0;
+int count = 0;
+while (number >= 1)
+{
+    int i = number % 10;
+    number = number / 10;
+    sum = sum + i;
+    count++;
+}
+Console.WriteLine("Сумма цифр в числе = " + sum);

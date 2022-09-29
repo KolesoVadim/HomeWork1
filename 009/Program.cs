b1d9bd7a9938d8dd4ddcbe8eb0922a01b18be324
KolesoVadim <vadimkolesnichenko1404@gmail.com>
2022-09-29 14:54:54 +0300

    added 009

---
+++ 009/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.Write("Enter your number: ");
+int number = int.Parse(Console.ReadLine()??"0");
+for (int i = 1; i <= number; i++)
+{
+    Console.WriteLine($"{i} * {i} * {i} = {i*i*i}");
+}

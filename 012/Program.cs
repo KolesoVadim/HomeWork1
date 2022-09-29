3189b9401f7a76769de7dbc179caf0b0c0c981fe
KolesoVadim <vadimkolesnichenko1404@gmail.com>
2022-09-29 16:31:49 +0300

    added 012

---
+++ 012/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int[] array = new int[8];
+for (int i = 0; i < 8; i++)
+{
+    Console.Write($"Enter {i+1} element of array: ");
+    array[i] = int.Parse(Console.ReadLine()??"0");
+}
+Console.Write("Your array: ");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
